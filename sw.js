69cb95f1b1b3fe5c5ed5ca91d7e9af0020cbf181
msstore95 <mustafa10sadi@gmail.com>
2026-05-02 23:26:09 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,11 @@
-const CACHE_NAME = 'ms-store-v2';
-const APP_SHELL = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
-
 self.addEventListener('install', event => {
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL).catch(() => null)));
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))).then(() => self.clients.claim())
-  );
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request).then(res => res || caches.match('./index.html'))));
-});
-
-self.addEventListener('message', event => {
-  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
+  event.respondWith(fetch(event.request));
 });
