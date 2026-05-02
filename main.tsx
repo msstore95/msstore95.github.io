044dde622935dc4324be26dbe46ba59ce883b49e
msstore95 <mustafa10sadi@gmail.com>
2026-05-03 02:03:19 +0300

    Add files via upload

--- main.tsx
+++ main.tsx
@@ -2,19 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./firebase-notifications.js";
 
-// 🔥 Firebase
-import { initializeApp } from "firebase/app";
-import { getMessaging, getToken, onMessage } from "firebase/messaging";
-
-// 🔥 إعدادات Firebase (نفس اللي عندك)
-const firebaseConfig = {
-  apiKey: "AIzaSyAn0y49jyaoLHyL6P7bce7IHcrsVNJ3ERM",
-  authDomain: "msstore-5c5f4.firebaseapp.com",
-  projectId: "msstore-5c5f4",
-  storageBucket: "msstore-5c5f4.firebasestorage.app",
-  messagingSenderId: "254756613602",
-  appId: "1:254756613602:web:YOUR_APP_ID"
-};
-
-// 🔥 تشغيل
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
