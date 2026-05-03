742ea1475c6f2191a3faf62e45ea4b694ab500e5
msstore95 <mustafa10sadi@gmail.com>
2026-05-03 05:12:09 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,11 +2,19 @@ importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyDakH4F8S6tmdSVTFENXMINk5oQABPSSVo",
+  apiKey: "AIzaSyDakH4F8S6tmdSVTFENMXInk5oQABPSSVo",
   authDomain: "msstore-5c5f4.firebaseapp.com",
   projectId: "msstore-5c5f4",
+  storageBucket: "msstore-5c5f4.appspot.com",
   messagingSenderId: "254756613602",
   appId: "1:254756613602:web:35825d0b88c3dae8545dbb"
 });
 
 const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: 'icon-192.png'
+  });
+});
